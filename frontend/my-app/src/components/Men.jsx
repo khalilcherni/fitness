5c80103db855e2a5637082455786f39d634dfe6b
khalilcherni <kcherni411@gmail.com>
2024-02-05 16:40:40 +0100

    20

--- frontend/my-app/src/components/Men.jsx
+++ frontend/my-app/src/components/Men.jsx
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios"
+import axios from 'axios';
+
 function Men() {
-const [data,setdata]=useState([])
+  const [data, setData] = useState([]);
 
-useEffect(()=>{
-axios.get("http://localhost:3001/api/get")
-.then(res=>setdata(res.data))
-.catch(err=>console.log(err))
-},[])
-
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/get')
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div>
-  {data.map((e)=>(
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-          <div className="card">
-            <img src={e.Image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{e.ExerciseName}</h5>
-              <p className="card-text">{e.Description}</p>
+    <div className="container mt-5">
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {data.map((e) => (
+          <div key={e.id} className="col mb-4">
+            <div className="card h-100">
+              <img src={e.Image} className="card-img-top" alt="Exercise" />
+              <div className="card-body">
+                <h5 className="card-title">{e.ExerciseName}</h5>
+                <p className="card-text">{e.Description}</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">Last updated: {e.DurationInMinutes}</small>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
-  ))}
     </div>
   );
 }
