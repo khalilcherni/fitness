430c5edb5c8a25626ed7af222f1bf5c5dac58f49
mohamedaziz14 <jo56501283@gmail.com>
2024-02-05 16:53:41 +0100

    5

--- frontend/my-app/src/components/Home.jsx
+++ frontend/my-app/src/components/Home.jsx
@@ -42,9 +42,36 @@ const Home = () => {
       </div>
 
       <footer className="section__container footer__container">
-        {/* Existing footer code */}
+        <span className="bg__blur"></span>
+        <span className="bg__blur footer__blur"></span>
+        <div className="footer__col">
+    
+          <p>
+            Take the first step towards a healthier, stronger you with our
+            unbeatable pricing plans. Let's sweat, achieve, and conquer together!
+          </p>
+          <div className="footer__socials">
+            <a href="#"><i className="ri-facebook-fill"></i></a>
+            <a href="#"><i className="ri-instagram-line"></i></a>
+            <a href="#"><i className="ri-twitter-fill"></i></a>
+          </div>
+        </div>
+       
+        <div className="footer__col">
+          <h4>About Us</h4>
+          <a href="#">Blogs</a>
+          <a href="#">Security</a>
+          <a href="#">Careers</a>
+        </div>
+        <div className="footer__col">
+          <h4>Contact</h4>
+          <a href="#">Contact Us</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms & Conditions</a>
+          
+        </div>
       </footer>
-
+      
       <div className="footer__bar">
         Copyright © 2024 Web fitness.
       </div>
