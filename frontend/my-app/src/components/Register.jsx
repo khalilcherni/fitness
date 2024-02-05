fd55873d250cf3e2afb0f8f3f7de0b6a0a499596
khalilcherni <kcherni411@gmail.com>
2024-02-05 20:30:48 +0100

    23

---
+++ frontend/my-app/src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+        <div>
+            <h2>Sign up</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Register
