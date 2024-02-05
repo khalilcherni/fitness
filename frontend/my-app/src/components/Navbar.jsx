a46a7259f7e44f6a4703cdff5ea834cf522c814c
khalilcherni <kcherni411@gmail.com>
2024-02-05 14:08:23 +0100

    10

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -10,7 +10,41 @@ function BasicExample() {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
-       
+      <Container>
+          <Navbar.Brand href="#home">
+            <img className='op' src="https://cdn-icons-png.flaticon.com/128/7006/7006898.png" alt="" height="30" />
+            Sicca Veneria
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="èbasic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" >
+              
+              </Nav.Link>
+              <Nav.Link href="#weather">
+                Weather
+              </Nav.Link>
+              <Nav.Link href="#Tourism">
+                Tourism
+              </Nav.Link>
+              <Nav.Link href="#Culture">
+                Culture
+              </Nav.Link>
+              <Nav.Link href="#Historic" >
+                Historic
+              </Nav.Link>
+              <NavDropdown title="Details" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#ContactForm">
+                  Book
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#Add" >
+                  Add
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+      
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
       <hr />
 
