0b5566aa94a57bd3de8c802af7510f9f9a435b84
khalilcherni <kcherni411@gmail.com>
2024-02-05 16:31:16 +0100

    18

--- frontend/my-app/src/components/Men.jsx
+++ frontend/my-app/src/components/Men.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
 function Men() {
+const [data,setdata]=useState([])
+
+useEffect(()=>{
+axios.get("http://localhost:3001/api/get")
+.then(res=>setdata(res.data))
+.catch(err=>console.log(err))
+},[])
+
+
   return (
-    <div>Men</div>
-  )
+    <div>
+  {data.map((e)=>(
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{e.ExerciseName}</h5>
+              <p className="card-text">{e.Description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  ))}
+    </div>
+  );
 }
 
-export default Men
+export default Men;
