471f9168d586b6c1187314609be9e49b0d5cfadb
khalilcherni <kcherni411@gmail.com>
2024-02-05 14:08:08 +0100

    9

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -9,7 +9,9 @@ function BasicExample() {
 
   return (
     <div>
-     
+      <Navbar expand="lg" className="bg-body-tertiary">
+       
+      </Navbar>
       <hr />
 
       {/* Render the received data only if searchClicked is true */}
