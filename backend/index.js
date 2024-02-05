afe241cf734cb8b569c302ae97c111ffdf9b2780
amina1amdouni <aminaamdouni01@gmail.com>
2024-02-05 16:06:39 +0100

    f

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
-const accRouter = require('./Routes/MenRoute')
+const PORT = 3001;
+const menRouter = require('./Routes/MenRoute')
+const womenRouter= require ('./Routes/WomenRoute')
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(cors());
 app.use(express.json());
-app.use('/api',accRouter)
+app.use('/api',menRouter)
+app.use('/api',womenRouter)
+
 // Use the routers corr
 
 app.get('/api', (req, res) => {
