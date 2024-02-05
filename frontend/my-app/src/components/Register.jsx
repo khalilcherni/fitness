859573a8e7964cb05fc42853bf6ab53f8d7c8f87
khalilcherni <kcherni411@gmail.com>
2024-02-05 20:34:19 +0100

    24

--- frontend/my-app/src/components/Register.jsx
+++ frontend/my-app/src/components/Register.jsx
@@ -5,7 +5,26 @@ function Register() {
     <div>
         <div>
             <h2>Sign up</h2>
+            <form action="">
+                <label htmlFor="name"><strong>Name</strong></label>
+                <input type="text" placeholder="name" id="name"/>
+            </form>
         </div>
+        <div>
+           
+            <form action="">
+                <label htmlFor="Email"><strong>Email</strong></label>
+                <input type="text" placeholder="Email" id="Email"/>
+            </form>
+        </div>
+        <div>
+        
+            <form action="">
+                <label htmlFor="Password"><strong>Password</strong></label>
+                <input type="text" placeholder="Password" id="Password"/>
+            </form>
+        </div>
+        <button type='submit'>Sign up</button>
     </div>
   )
 }
