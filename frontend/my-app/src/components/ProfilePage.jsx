b818b960fe1136f369cb1369c3cd0cc38a88cafc
khalilcherni <kcherni411@gmail.com>
2024-02-08 11:21:39 +0100

    100

--- frontend/my-app/src/components/ProfilePage.jsx
+++ frontend/my-app/src/components/ProfilePage.jsx
@@ -18,13 +18,14 @@ import {
 } from 'mdb-react-ui-kit';
 import './ProfilePage.css'
 import { useState } from 'react';
+import Home from './Home'
 
 export default function ProfilePage() {
   const [fullName, setFullName] = useState('John Doe');
   const [email, setEmail] = useState('johndoe@example.com');
   const [location, setLocation] = useState('Fitness Street, Gym City, CA');
   const [imageSrc, setImageSrc] = useState('https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp');
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // Functions to handle changes in user information
   const handleFullNameChange = (event) => {
     setFullName(event.target.value);
@@ -37,8 +38,12 @@ export default function ProfilePage() {
   const handleImageChange = (event) => {
     setImageSrc(event.target.value);
   }
+// Set isLoggedIn to true to render the Home component
+  
   return (
     <section className='o'>
+               
+
       <MDBContainer className="py">
 
         <MDBRow >
@@ -214,6 +219,10 @@ export default function ProfilePage() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
+     
+      
+     
     </section>
+
   );
 }
