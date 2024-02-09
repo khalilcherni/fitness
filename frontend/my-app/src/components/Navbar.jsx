b2375f95e44d339de8bf1430bfeb745cf19a4a49
amina1amdouni <aminaamdouni01@gmail.com>
2024-02-09 09:11:49 +0100

    done

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import Home from './Home';
 import Men from './Men';
-import AccessoireSport from './Accessoire Sport';
+import AccessoirSport from './Accessoire Sport'
 import Register from './Register';
 import './Navbar.css';
 import Lose from './lose';
 import Gain from './gain';
 import ContactForm from './ContactUs';
+import ProfilePage from './ProfilePage';
 import AboutUs from './AboutUs';
-import Proteine from './Proteins';
-
-
-
 import CartList from './CartList';
 import Add from './AddForMen';
 import Women from './Women';
@@ -53,6 +50,7 @@ function BasicExample() {
               <Nav.Link href="#home" onClick={() => handleTabClick('home')}>
                 Home
               </Nav.Link>
+          
     
               <NavDropdown title="Exercise" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#Men" onClick={() => handleTabClick('Men')}>
@@ -90,8 +88,7 @@ function BasicExample() {
               </NavDropdown>
 
               <Nav.Link href="#aboutUs" onClick={() => handleTabClick('aboutUs')}>
-                About 
-           
+          About 
               </Nav.Link>
 
               <Nav.Link id='nb' href="#User" onClick={() => handleTabClick('Register')}>
@@ -103,15 +100,10 @@ ProfilePage
   
     
             </Nav>
-          
-            <Nav className="ms-auto">
-              
-            </Nav>
           </Navbar.Collapse>
           <Nav.Link id='a'  href="#Cart" onClick={() => handleTabClick('Cart')}>
               <img className="o" src="https://cdn-icons-png.flaticon.com/128/3643/3643914.png" alt="" height="30" />
 </Nav.Link>
-
         </Container>
  
       </Navbar>
@@ -120,24 +112,14 @@ ProfilePage
       {activeTab === 'home' && <Home />}
       {activeTab === 'aboutUs' && <AboutUs />}
       {activeTab === 'Men' && <Men />}
-       {activeTab === 'Register' && <Register />}
       {activeTab === 'Women' && <Women />}
-        
       {activeTab === 'AddForWomen' && <AddForWomen />}
-
-      {activeTab === 'AddForMen' && <Add />}
-    
-
       {activeTab === 'Register' && <Register />}
-      {activeTab === 'proteins' && <Proteine />}
-      {activeTab === 'accessoireSport' && <AccessoireSport />}
-      
-
       {activeTab === 'loss weight' && <Lose />}
       {activeTab === 'gain Weight' && <Gain />}
       {activeTab === 'Contact' && <ContactForm />}
       {activeTab === 'Cart' && <CartList />}
-   
+      {activeTab === 'AddForMen' && <Add />}
 
       {/* {activeTab === 'User' && <Register />} */}
     </div>
@@ -145,4 +127,3 @@ ProfilePage
 }
 
 export default BasicExample;
-
