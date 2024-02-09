f9fa0b18570d9824f6813ac2f9976e7f71fb647d
amina1amdouni <aminaamdouni01@gmail.com>
2024-02-09 11:19:20 +0100

    css gain done

--- frontend/my-app/src/components/gain.jsx
+++ frontend/my-app/src/components/gain.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Men.css';
+import './gain.css';
 
 function Gain() {
   const [data, setData] = useState([]);
